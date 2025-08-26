7ffe1c3e33f1bb49e3f3b30d1fbccf8015e9dfbb
robinmin <luonbin@hotmail.com>
2025-08-26 14:03:14 -0700

    fix: remove unsed code

--- faster/core/auth/schemas.py
+++ faster/core/auth/schemas.py
@@ -3,15 +3,10 @@ from typing import Any
 from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
 
-class UserBase(BaseModel):
-    """Base schema for user properties."""
-
-    email: EmailStr = Field(..., description="User's email address.")
-
-
-class AuthUser(UserBase):
+class AuthUser(BaseModel):
     """Schema for authenticated user."""
 
+    email: EmailStr = Field(..., description="User's email address.")
     id: str
     token: str
     raw: dict[str, Any]
