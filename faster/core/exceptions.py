6ef8c4a2b792ca54da0bddcf60789d3f49386faf
robinmin <luonbin@hotmail.com>
2025-08-27 20:20:24 -0700

    fix: exception handling

--- faster/core/exceptions.py
+++ faster/core/exceptions.py
@@ -3,15 +3,17 @@ from typing import Any
 
 from fastapi import Request, status
 from fastapi.exceptions import RequestValidationError
-from starlette.exceptions import HTTPException as StarletteHTTPError
 
-from faster.core.schemas import APIResponse
+from .logger import get_logger
+from .schemas import AppResponse
+
+logger = get_logger(__name__)
 
 
 ###############################################################################
 ## Define all exception classes
 ###############################################################################
-class APIError(Exception):
+class AppError(Exception):
     """Base class for all application exceptions."""
 
     def __init__(
@@ -26,44 +28,43 @@ class APIError(Exception):
         self.errors = errors or []
 
 
-class HTTPError(APIError):
+class HTTPError(AppError):
     """Custom HTTP exception class."""
 
     def __init__(self, message: str, status_code: int, errors: list[dict[str, Any]] | None = None) -> None:
         super().__init__(message, status_code, errors)
 
 
-class ValidationError(APIError):
+class ValidationError(AppError):
     """Custom validation exception class."""
 
     def __init__(self, errors: list[dict[str, Any]]) -> None:
-        super().__init__("Validation error", status_code=422, errors=errors)
+        super().__init__("Validation error", status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, errors=errors)
 
 
-class DBError(APIError):
+class DBError(AppError):
     """Custom DB exception for uniform error handling."""
 
 
-class AuthError(APIError):
+class AuthError(AppError):
     """Custom DB exception for authorization error."""
+
+    def __init__(
+        self,
+        message: str,
+        status_code: int = status.HTTP_401_UNAUTHORIZED,
+        errors: list[dict[str, Any]] | None = None,
+    ) -> None:
+        super().__init__(message, status_code, errors)
 
 
 ###############################################################################
 ##  Define all exception  handlers
 ###############################################################################
-async def default_exception_handler(request: Request, exc: Exception) -> APIResponse[Any]:
-    """Global exception handler for uncaught exceptions."""
-    return APIResponse(
-        status="error",
-        message="Internal server error",
-        headers={"X-Request-ID": request.headers.get("X-Request-ID") or ""},
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-    )
-
-
-async def api_exception_handler(_: Request, exc: APIError) -> APIResponse[Any]:
+async def app_exception_handler(_: Request, exc: AppError) -> AppResponse[Any]:
     """Global exception handler for APIError."""
-    return APIResponse(
+    logger.error("Application error: [%d] %s - %s", exc.status_code, exc.message, exc.errors if exc.errors else None)
+    return AppResponse(
         status="error",
         message=exc.message,
         status_code=exc.status_code,
@@ -71,16 +72,7 @@ async def api_exception_handler(_: Request, exc: APIError) -> APIResponse[Any]:
     )
 
 
-async def http_exception_handler(_: Request, exc: StarletteHTTPError) -> APIResponse[Any]:
-    """Global exception handler for Starlette/FastAPI HTTPException."""
-    return APIResponse(
-        status="http error",
-        message=exc.detail,
-        status_code=exc.status_code,
-    )
-
-
-async def custom_validation_exception_handler(_: Request, exc: RequestValidationError) -> APIResponse[Any]:
+async def custom_validation_exception_handler(_: Request, exc: RequestValidationError) -> AppResponse[Any]:
     """
     Custom global exception handler for Pydantic's RequestValidationError.
     This handler formats the validation errors to be more developer-friendly.
@@ -89,28 +81,19 @@ async def custom_validation_exception_handler(_: Request, exc: RequestValidation
     for error in exc.errors():
         field = ".".join(map(str, error["loc"])) if error["loc"] else "general"
         error_details[field].append(error["msg"])
-
-    return APIResponse(
+    logger.error("Validation error: %s", error_details)
+    return AppResponse(
         status="validation error",
         message="Request validation failed",
-        status_code=422,
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         data=[{"field": k, "messages": v} for k, v in error_details.items()],
     )
 
 
-async def db_exception_handler(request: Request, exp: DBError) -> APIResponse[Any]:
-    """Global exception handler for DBError."""
-    return APIResponse(
-        status="db error",
-        message=exp.message,
-        status_code=exp.status_code,
-        data=exp.errors if exp.errors else None,
-    )
-
-
-async def auth_exception_handler(request: Request, exp: DBError) -> APIResponse[Any]:
-    """Global exception handler for DBError."""
-    return APIResponse(
+async def auth_exception_handler(request: Request, exp: AuthError) -> AppResponse[Any]:
+    """Global exception handler for AuthError."""
+    logger.error("Authentication error: [%d] %s - %s", exp.status_code, exp.message, exp.errors if exp.errors else None)
+    return AppResponse(
         status="Authentication failed",
         message=exp.message,
         status_code=exp.status_code,
