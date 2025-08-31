d2cf62bc3398d73044a566dae9a226e1ecc4f933
robinmin <luonbin@hotmail.com>
2025-08-30 21:13:08 -0700

    feat: add new utility function is_api_call

--- faster/core/utilities.py
+++ faster/core/utilities.py
@@ -4,6 +4,13 @@ from fastapi import FastAPI, Request
 from fastapi.routing import APIRoute
 
 
+def is_api_call(request: Request) -> bool:
+    """Check if the request is an API call by checking the Accept header."""
+
+    accept_header = request.headers.get("accept")
+    return (accept_header and "application/json" in accept_header) or False
+
+
 def get_all_endpoints(app: FastAPI) -> list[dict[str, Any]]:
     """
     Return a list of all endpoints with method, path, tags, and function name.
@@ -39,8 +46,9 @@ def get_current_endpoint(request: Request, endpoints: list[dict[str, Any]]) -> d
     request_path = request.url.path
     request_method = request.method.upper()
 
+    # TODO: We enabled HEAD method support here, need to check if it's necessary to balance the convinence and security
     for ep in endpoints:
-        if request_path == ep.get("path") and request_method in ep.get("methods", []):
+        if request_path == ep.get("path") and (request_method == "HEAD" or request_method in ep.get("methods", [])):
             return ep
 
     return None
